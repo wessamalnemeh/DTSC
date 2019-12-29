9cc07a0440e1396c689c3c72e90bad04fdcd1f5f
Wessam Alnemeh <wessaa92@zedat.fu-berlin.de>
2019-12-29 18:26:30 +0100

    add heatmap

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-import { tileLayer, latLng } from 'leaflet';
+import { tileLayer, latLng, Map, LatLng } from 'leaflet';
+import 'leaflet';
+import 'leaflet.heat'
+declare let L;
+import { addressPoints } from '../assets/realworld'
+
+
 
 @Component({
   selector: 'app-root',
@@ -10,10 +16,24 @@ export class AppComponent {
   title = 'Frontend';
   options = {
     layers: [
-      tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+      L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 18,
+        attribution: ""
+      })
     ],
-    zoom: 4,
-    center: latLng(46.879966, -121.726909)
+    zoom: 5,
+    center: L.latLng(-37.87, 175.475)
   };
+
+  onMapReady(map: Map) {
+    // Do stuff with map
+    var addresses=[]
+    addresses.push( latLng(46.879966, -121.726909))
+    addresses.push( latLng(46.879966, -122.726909))
+    addresses.push( latLng(46.879966, -120.726909))
+    let newAddressPoints = addressPoints.map(function (p) { return [p[0], p[1]]; });
+    const heat = L.heatLayer(newAddressPoints).addTo(map);
+
+  } 
  
 }
